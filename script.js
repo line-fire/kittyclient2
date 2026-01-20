fe14f55645dd6d54cdec8ef25684373be63651fe
line-fire <5al09yfy@gmail.com>
2026-01-20 03:41:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// GitHub'daki dosyanın içeriği:
+console.log("Selam! Ben GitHub'dan geldim.");
+const os = require('os');
+console.log("Senin bilgisayarın: " + os.hostname());
+// Hatta buradan child_process ile exe bile açtırabilirsin
